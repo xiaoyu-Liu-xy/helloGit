9c7475759d28f07550ab2eb77755403006f75d1b
liuxiaoyu <907648980@qq.com>
2022-05-22 18:43:30 +0800

    终于对了

--- HelloGit/src/main/java/com/lxy/git/HelloGit.java
+++ HelloGit/src/main/java/com/lxy/git/HelloGit.java
@@ -3,8 +3,6 @@ package com.lxy.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("更新1");
-        System.out.println("创建分支");
-        System.out.println("你怎么回事，啊啊啊啊");
+
     }
 }
