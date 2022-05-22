2f05d902c3c07c51232cc273baa5f73ed0f609f7
liuxiaoyu <907648980@qq.com>
2022-05-22 18:15:10 +0800

    添加文件

---
+++ hello/src/main/java/com/lxy/Hello.java
@@ -0,0 +1,7 @@
+package com.lxy;
+
+public class Hello {
+    public String sayHello(String name){
+        return "Hello "+name+"!";
+    }
+}
---
+++ src/sy/helloworld.java
@@ -0,0 +1,11 @@
+package sy;
+
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("刘效余真帅");
+
+    }
+}
+
+
+
