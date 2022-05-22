60f817eeeb9edf5aa023cc45c93f2caa3b8c9de1
liuxiaoyu <63048777+xiaoyu-Liu-xy@users.noreply.github.com>
2022-05-22 19:37:15 +0800

    Update HelloGit.java
    在线添加

--- HelloGit/src/main/java/com/lxy/git/HelloGit.java
+++ HelloGit/src/main/java/com/lxy/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("主干添加");
         System.out.println("分支添加");
+        System.out.println("在线添加");
     }
 }
